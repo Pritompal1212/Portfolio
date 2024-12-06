ad441e3b722347ef853f976d6ff9b42eb657d5c2
Pritompal1212 <mspritom111@gmail.com>
2024-12-06 14:35:06 +0530

    added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,52 +1,111 @@
-
+// import React from 'react';
+// import 'E:\react\final\src\components\services.css';
 const Services = () => {
   return (
-    <div className="container">
-      <h3>My Services</h3>
-      <div className="row row-cols-1 row-cols-lg-3 justify-content-around align-items-center gap-4 mt-4">
-        <div
-          className="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center"
-          style={{
-            height: "300px",
-            width: "400px",
-            backgroundImage: "url('unsplash-photo-1.jpg')"
-          }}
+    // <div classNameName="container">
+    //   <h3>My Services</h3>
+    //   <div classNameName="row row-cols-1 row-cols-lg-3 justify-content-around align-items-center gap-4 mt-4">
+    //     <div
+    //       classNameName="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center"
+    //       style={{
+    //         height: "300px",
+    //         width: "400px",
+    //         backgroundImage: "url('unsplash-photo-1.jpg')"
+    //       }}
           
-          >
-          Frontend Developer
-        </div>
-        <div
-          className="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center"
-          style={{
-            height: "300px",
-            width: "400px",
-            backgroundImage: "url('unsplash-photo-1.jpg')"
-          }}
-          >
-          Frontend Developer
-        </div>
-        <div
-          className="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center"
-          style={{
-            height: "300px",
-            width: "400px",
-            backgroundImage: "url('unsplash-photo-1.jpg')"
-          }}
-        >
-          Frontend Developer
-        </div>
-      </div>
+    //       >
+    //       Frontend Developer
+    //     </div>
+    //     <div
+    //       classNameName="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center"
+    //       style={{
+    //         height: "300px",
+    //         width: "400px",
+    //         backgroundImage: "url('unsplash-photo-1.jpg')"
+    //       }}
+    //       >
+    //       Frontend Developer
+    //     </div>
+    //     <div
+    //       classNameName="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center"
+    //       style={{
+    //         height: "300px",
+    //         width: "400px",
+    //         backgroundImage: "url('unsplash-photo-1.jpg')"
+    //       }}
+    //     >
+    //       Frontend Developer
+    //     </div>
+    //   </div>
 
-      {/* CSS styles */}
-      <style jsx>{`
-        @media (max-width: 768px) {
-          .col.card-cover {
-            height: 320px !important;
-            width: 284px !important;
-          }
-        }
-      `}</style>
+    //   {/* CSS styles */}
+    //   <style jsx>{`
+    //     @media (max-width: 768px) {
+    //       .col.card-cover {
+    //         height: 320px !important;
+    //         width: 284px !important;
+    //       }
+    //     }
+    //   `}</style>
+    // </div>
+
+    <div id="services">
+    <div className="container" >
+        <h1 classNameName="sub-title">My Services</h1>
+        <div className="services-list">
+            <div >
+                <i className="fas fa-code"> </i>
+                <h2>Full Stact development</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div>
+                <i className="fas fa-code"> </i>
+                <h2>UI/UX Design</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div>
+                <i className="fas fa-code"> </i>
+                <h2>Front-end development</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div>
+                <i className="fas fa-code"> </i>
+                <h2>Back-end development</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div>
+                <i className="fas fa-code"> </i>
+                <h2>App development</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div>
+                <i className="fas fa-code"> </i>
+                <h2>MERN development</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div>
+                <i className="fas fa-code"> </i>
+                <h2>Software development</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+            <div>
+                <i className="fas fa-code"> </i>
+                <h2>Digital Marketing</h2>
+                <p>We are the most sought-after Web Design & Digital Marketing Company in Bhubaneswar, Odisha. We provide services like Web design & development, SEO , Social media …</p>
+                <a href="#">Learn more</a>
+            </div>
+        </div>  
     </div>
+</div>
+
+
   );
 };
 
@@ -56,11 +115,11 @@ export default Services;
 
     // return (
     //     <>
-    //         <div className="container">
+    //         <div classNameName="container">
     //             <h3>My Services</h3>
                 
-    //             <div className="row row-cols-1 row-cols-lg-3 justify-content-around align-items-center gap-4">
-    //                 <div className="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center" style={{
+    //             <div classNameName="row row-cols-1 row-cols-lg-3 justify-content-around align-items-center gap-4">
+    //                 <div classNameName="col card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center" style={{
     //                     // height: "320px",
     //                     // width: "284px"
     //                     // this is for small device
@@ -69,11 +128,11 @@ export default Services;
                         
 
     //                 }} >Frontend Devoloper</div>
-    //                 <div className="col card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center" style={{
+    //                 <div classNameName="col card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center" style={{
     //                     height: "300px",
     //                     width: "400px"
     //                 }} >Frontend Devoloper</div>
-    //                 <div className="col card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center" style={{
+    //                 <div classNameName="col card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg d-flex justify-content-center align-items-center" style={{
     //                     height: "300px",
     //                     width: "400px"
     //                 }} >Frontend Devoloper</div>
