7e0c52c01346d81923a0ab82ad5aee75992ffddd
OMSAH <huncyoms@gmail.com>
2024-12-14 11:37:16 -0500

    added by omsah

---
+++ src/components/Education.jsx
@@ -0,0 +1,75 @@
+export default function Education() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>My Education</h2>
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-12 col-md-6"
+            style={{ borderLeft: "1px solid blue", position: "relative" }}
+          >
+            <div style={{border:"2px solid red", margin:"10px", padding:"10px"}}>
+              <p>data</p>
+              <h6>heading</h6>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium commodi enim adipisci delectus, at quos fuga
+                asperiores, excepturi aliquid aut ipsa.
+              </p>
+            </div>
+
+            <div className="custom-circle-top"></div>
+          </div>
+          <div
+            className="col-12 col-md-6"
+            style={{ borderLeft: "1px solid blue", position: "relative" }}
+          >
+            <div style={{border:"2px solid red", margin:"10px", padding:"10px"}}>
+              <p>data</p>
+              <h6>heading</h6>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium commodi enim adipisci delectus, at quos fuga
+                asperiores, excepturi aliquid aut ipsa.
+              </p>
+            </div>
+
+            <div className="custom-circle-top"></div>
+          </div>
+          <div
+            className="col-12 col-md-6"
+            style={{ borderLeft: "1px solid blue", position: "relative" }}
+          >
+            <div style={{border:"2px solid red", margin:"10px", padding:"10px"}}>
+              <p>data</p>
+              <h6>heading</h6>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium commodi enim adipisci delectus, at quos fuga
+                asperiores, excepturi aliquid aut ipsa.
+              </p>
+            </div>
+
+            <div className="custom-circle-top"></div>
+          </div>
+          <div
+            className="col-12 col-md-6"
+            style={{ borderLeft: "1px solid blue", position: "relative" }}
+          >
+            <div style={{border:"2px solid red", margin:"10px", padding:"10px"}}>
+              <p>data</p>
+              <h6>heading</h6>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium commodi enim adipisci delectus, at quos fuga
+                asperiores, excepturi aliquid aut ipsa.
+              </p>
+            </div>
+
+            <div className="custom-circle-top"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
